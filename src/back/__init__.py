0db9aa85b2fe00bcc187bf54b055fb475d5aa612
unai <unaidieguez03@gmail.com>
2025-01-23 11:53:49 +0100

    port

--- src/back/__init__.py
+++ src/back/__init__.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 def start_server():
-    uvicorn.run("back.api.model_controller:_app", app_dir="src/", host="0.0.0.0", port=8080, log_level="info", reload=True)
+    uvicorn.run("back.api.model_controller:_app", app_dir="src/", host="0.0.0.0", port=8000, log_level="info", reload=True)
